196f75b1d87e7e28ea35bef105c826f714b5cb72
Filipe Colaquecez <filipepcolaquecez@gmail.com>
2022-04-02 02:04:55 -0300

    feat: text component

---
+++ src/components/Text/Text.component.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { TextProps } from 'react-native';
+import * as S from './Text.styles';
+
+interface IText extends TextProps {
+  color?: string;
+  marginBottom?: number;
+  marginTop?: number;
+  marginLeft?: number;
+  marginRight?: number;
+  fontSize?: number;
+  fontWeight?: string;
+  textTransform?: React.CSSProperties['textTransform'];
+  children: React.ReactNode;
+}
+
+const Text = ({
+  color = '#000000',
+  marginBottom = 0,
+  marginLeft = 0,
+  marginRight = 0,
+  fontWeight = '400',
+  marginTop = 0,
+  fontSize = 14,
+  textTransform = 'capitalize',
+  children,
+  ...rest
+}: IText) => {
+  return (
+    <S.TextStyle
+      fontSize={fontSize}
+      color={color}
+      fontWeight={fontWeight}
+      textTransform={textTransform}
+      marginBottom={marginBottom}
+      marginTop={marginTop}
+      marginLeft={marginLeft}
+      marginRight={marginRight}
+      {...rest}
+    >
+      {children}
+    </S.TextStyle>
+  );
+};
+
+export default Text;
---
+++ src/components/Text/Text.styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components/native';
+
+interface IText {
+  marginTop: number;
+  marginBottom: number;
+  marginLeft: number;
+  marginRight: number;
+  fontSize: number;
+  color: string;
+  textTransform: string;
+  fontWeight: string;
+}
+
+export const TextStyle = styled.Text<IText>`
+  margin-top: ${({ marginTop }) => marginTop}px;
+  margin-bottom: ${({ marginBottom }) => marginBottom}px;
+  margin-left: ${({ marginLeft }) => marginLeft}px;
+  margin-right: ${({ marginRight }) => marginRight}px;
+  color: ${({ color }) => color};
+  font-weight: ${({ fontWeight }) => fontWeight};
+  text-transform: ${({ textTransform }) => textTransform};
+  font-size: ${({ fontSize }) => fontSize}px;
+`;
