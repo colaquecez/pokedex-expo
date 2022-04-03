8243826e4b522209485c39ef0598ee8b9908169b
Filipe Colaquecez <filipepcolaquecez@gmail.com>
2022-04-03 19:09:20 -0300

    feat: HomeScreen

---
+++ src/screens/Home/Home.screens.tsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import { FlatList, Dimensions } from 'react-native';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+import { useLazyGetPokemonsQuery } from 'src/redux/pokemon/pokemon.api';
+import { PokemonResult } from 'src/redux/pokemon/pokemon.types';
+import { CardPokemon, Text, Error, Loading, Layout } from 'src/components';
+import { RootStackParamList } from 'src/shared/routes/Main.routes';
+import columnWrapperStyle from 'src/styles/columWrapperListPokemon';
+import useAppSelector from 'src/hook/useSelector';
+
+import Pokeball from '../../../assets/pokebolaoverlay.svg';
+import * as S from './Home.styles';
+
+type NavigationProps = NativeStackScreenProps<RootStackParamList, 'ListHome'>;
+
+const Home = ({ navigation }: NavigationProps) => {
+  const [page, setPage] = useState(0);
+  const [results, setResults] = useState<PokemonResult[]>([]);
+  const { favorites } = useAppSelector((state) => state.pokemon);
+
+  const [
+    triggerGetPokemon,
+    {
+      data: getPokemon,
+      isError: getPokemonIsError,
+      isFetching: getPokemonIsLoading
+    }
+  ] = useLazyGetPokemonsQuery();
+
+  useEffect(() => {
+    triggerGetPokemon(page);
+  }, [page]);
+
+  useEffect(() => {
+    setResults(getPokemon ? [...results, ...getPokemon?.results] : []);
+  }, [getPokemon]);
+
+  const { width: widthScreen } = Dimensions.get('window');
+
+  if (getPokemonIsError) {
+    return <Error refetch={() => triggerGetPokemon(page)} />;
+  }
+
+  return (
+    <Layout>
+      <S.ContainerSvg>
+        <Pokeball width={250} height={250} />
+      </S.ContainerSvg>
+      <FlatList
+        ListFooterComponent={getPokemonIsLoading ? <Loading /> : null}
+        ListHeaderComponent={
+          <Text
+            fontWeight="700"
+            fontSize={32}
+            marginLeft={16}
+            marginBottom={24}
+            marginTop={32}
+          >
+            Pokedex
+          </Text>
+        }
+        data={results}
+        numColumns={2}
+        onEndReached={() => {
+          setPage((state) => state + 20);
+        }}
+        onEndReachedThreshold={0.01}
+        showsVerticalScrollIndicator={false}
+        columnWrapperStyle={columnWrapperStyle}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => {
+          return (
+            <CardPokemon
+              isFavorite={favorites.some((favorite) => favorite.id === item.id)}
+              onPress={() => navigation.navigate('Details', item)}
+              image={item.image}
+              width={widthScreen / 2 - 16}
+              name={item.name}
+            />
+          );
+        }}
+      />
+    </Layout>
+  );
+};
+
+export default Home;
---
+++ src/screens/Home/Home.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+
+export const ContainerSvg = styled.View`
+  position: absolute;
+  right: 0px;
+  top: -45px;
+`;
