bcbd96896093da8a5667d0fb24e1e097c1593a78
Filipe Colaquecez <filipepcolaquecez@gmail.com>
2022-04-03 16:24:50 -0300

    feat: provider jest to test the components with theme

---
+++ src/components/ProviderThemeJest/ProviderThemeJest.component.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
+import { colors } from '../../styles/theme';
+
+interface IProviderThemeJest {
+  children: React.ReactNode;
+}
+
+const ProviderThemeJest = ({ children }: IProviderThemeJest) => {
+  return <ThemeProvider theme={colors}>{children}</ThemeProvider>;
+};
+
+export default ProviderThemeJest;
