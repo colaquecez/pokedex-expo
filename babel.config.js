d1bc4919d7ae013bba61fbe233a7475f7ab0e243
Filipe Colaquecez <filipepcolaquecez@gmail.com>
2022-04-03 18:54:48 -0300

    feat: setup path absolute

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            src: './src',
+            assets: './assets'
+          }
+        }
+      ]
+    ]
   };
 };
