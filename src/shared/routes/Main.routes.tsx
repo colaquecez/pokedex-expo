a1fde52b1d857f4165aec3e4f98d38f5c09e564c
Filipe Colaquecez <filipepcolaquecez@gmail.com>
2022-04-03 02:27:45 -0300

    feat: bottom navigation

--- src/shared/routes/Main.routes.tsx
+++ src/shared/routes/Main.routes.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Home } from '../../screens/';
+import { Details } from '../../screens/';
+import MenuRouter from '../routes/Menu.routes';
 
-const MainStack = createNativeStackNavigator();
+export type RootStackParamList = {
+  ListHome: undefined;
+  Details: { name: string; url: string; image: string };
+};
 
-const RootStackScreen = () => {
+const MainStack = createNativeStackNavigator<RootStackParamList>();
+
+const MainStackRoute = () => {
   return (
     <MainStack.Navigator>
-      <MainStack.Screen name="Home" component={Home} />
+      <MainStack.Screen
+        options={{
+          headerShown: false
+        }}
+        name="ListHome"
+        component={MenuRouter}
+      />
+      <MainStack.Screen
+        options={{
+          animation: 'slide_from_bottom',
+          headerTitle: '',
+          headerShown: false
+        }}
+        component={Details}
+        name="Details"
+      />
     </MainStack.Navigator>
   );
 };
 
-export default RootStackScreen;
+export default MainStackRoute;
---
+++ src/shared/routes/Menu.routes.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { Home, Favorites } from '../../screens/';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+export type RootStackParamList = {
+  Home: undefined;
+  Details: { name: string; url: string; image: string };
+};
+
+const MenuTabBar = createBottomTabNavigator();
+
+const MenuTabBarRoute = () => {
+  return (
+    <MenuTabBar.Navigator>
+      <MenuTabBar.Screen
+        options={{
+          tabBarLabel: 'Pokemons',
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons
+              name="pokemon-go"
+              size={size}
+              color={color}
+            />
+          ),
+          headerShown: false
+        }}
+        name="Home"
+        component={Home}
+      />
+      <MenuTabBar.Screen
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name="heart" size={size} color={color} />
+          )
+        }}
+        name="Favorites"
+        component={Favorites}
+      />
+    </MenuTabBar.Navigator>
+  );
+};
+
+export default MenuTabBarRoute;
