e590d374de4541196af608bedff68e2bbe55b5fc
Filipe Colaquecez <filipepcolaquecez@gmail.com>
2022-04-03 16:24:23 -0300

    feat: style to use on flatlist 2 columns

---
+++ src/styles/columWrapperListPokemon.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const column = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    marginBottom: 8,
+    marginHorizontal: 10,
+    justifyContent: 'space-between'
+  }
+});
+
+export default column.wrapper;
