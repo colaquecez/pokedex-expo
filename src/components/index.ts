9bd0d71004682461ac7030d2192eeba5b95d0415
Filipe Colaquecez <filipepcolaquecez@gmail.com>
2022-04-03 16:25:19 -0300

    fear: index to export the components easier

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as CardPokemon } from './CardPokemon/CardPokemon.component';
+export { default as Text } from './Text/Text.component';
+export { default as PokemonNotFound } from './PokemonNotFound/PokemonNotFound.component';
+export { default as Tag } from './Tag/Tag.component';
+export { default as Error } from './Error/Error.component';
+export { default as Loading } from './Loading/Loading.component';
+export { default as Input } from './Input/Input.component';
+export { default as ProviderThemeJest } from './ProviderThemeJest/ProviderThemeJest.component';
